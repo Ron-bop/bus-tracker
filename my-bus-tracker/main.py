ffe4afe39295591f6d9642955c3b00fdb90dfbdf
Ronald Blackwell <naldoblackwell14@gmail.com>
2025-07-16 00:23:08 +1000

    git add . git commit -m "made function to track bus and train" git push

--- my-bus-tracker/main.py
+++ my-bus-tracker/main.py
@@ -3,66 +3,86 @@ from google.transit import gtfs_realtime_pb2
 from datetime import datetime
 
 # API dictionary
-url = "https://gtfsrt.api.translink.com.au/api/realtime/SEQ/TripUpdates/Bus"
+bus_url = "https://gtfsrt.api.translink.com.au/api/realtime/SEQ/TripUpdates/Bus"
+train_url = "https://gtfsrt.api.translink.com.au/api/realtime/SEQ/TripUpdates/Rail"
 
 # Allows us to request the API from the website (updates constantly)
-api_response = requests.get(url)
+bus_response = requests.get(bus_url)
+train_response = requests.get(train_url)
 
 # Parse the GTFS-RT feed
+# BUS:
 bus_feed = gtfs_realtime_pb2.FeedMessage()
-bus_feed.ParseFromString(api_response.content)
+bus_feed.ParseFromString(bus_response.content)
+
+# TRAIN:
+train_feed = gtfs_realtime_pb2.FeedMessage()
+train_feed.ParseFromString(train_response.content)
 
 # Variables: grabbed from Translink GTFS data
+# BUS
 bus_340 = "340-4158"
-beams_rd_bus_stop = "4069"
+bus_stop = "4069" # Carseldine Station bound
 
-# current time
-time_now = datetime.now().replace(second = 0, microsecond = 0)
+# TRAIN
+# 600450,600450,"Carseldine station, platform 2",, -27.347310,
+# RPSP Springfield Central station, platform one 600443,600443,"Geebung station, platform 1",, -27.368740
+train_route_id = "RPSP-4266"
+station = "600443"
+
+
 
 # timestamp - only show minutes
 def minute_timestamp(timestamp):
     return datetime.fromtimestamp(timestamp).replace(second=0, microsecond=0)
 
-# track soonest upcoming bus arrival
-next_time_difference = None
-closest_bus = None
-closest_stop_time_update = None
-closest_estimated_arrival = None
 
-# Loop through trip updates and print info
-for bus_route in bus_feed.entity:
-    if bus_route.HasField('trip_update'):
-        bus_update = bus_route.trip_update
-        if bus_update.trip.route_id == bus_340:
+def tracker(feed, vehicle_id, stop_number):
+    # current time
+    time_now = datetime.now().replace(second=0, microsecond=0)
 
-            for stop_time_update in bus_update.stop_time_update:
-                if (
-                    stop_time_update.stop_id == beams_rd_bus_stop and
-                    stop_time_update.HasField('arrival') and
-                    stop_time_update.arrival.HasField('time')
-                    ):
+    # track next upcoming vehicle
+    next_time_difference = None
+    closest_vehicle = None
+    closest_stop_time_update = None
+    closest_estimated_arrival = None
 
-                    estimated_arrival = minute_timestamp(stop_time_update.arrival.time)
-                    time_difference = estimated_arrival - time_now
+    # Loop through trip updates and print info
+    for route in feed.entity:
+        if route.HasField('trip_update'):
+            vehicle_update = route.trip_update
+            if vehicle_update.trip.route_id == vehicle_id:
 
-                    if (estimated_arrival > time_now and
-                        ((next_time_difference is None) or (time_difference < next_time_difference))):
-                            next_time_difference = time_difference
-                            closest_bus = bus_update
-                            closest_stop_time_update = stop_time_update
-                            closest_estimated_arrival = estimated_arrival
+                for stop_time_update in vehicle_update.stop_time_update:
+                    if (
+                        stop_time_update.stop_id == stop_number and
+                        stop_time_update.HasField('arrival') and
+                        stop_time_update.arrival.HasField('time')
+                        ):
 
-if closest_bus and closest_stop_time_update:
-    print(f"Trip ID: {closest_bus.trip.trip_id}")
-    print(f"Route ID: {closest_bus.trip.route_id}")
-    time_difference_miunutes = int(next_time_difference.total_seconds() // 60)
-    if time_difference_miunutes > 1:
-        print(f"Your Bus will be arriving in {time_difference_miunutes} minutes")
-    else:
-        print(f"Your Bus will be arriving in {time_difference_miunutes} minute")
-    print(f"ETA: {closest_estimated_arrival}")
-    print(f"Stop ID: Beams Rd {closest_stop_time_update.stop_id}")
-    print(f"Arrival delay: {closest_stop_time_update.arrival.delay}")
+                        estimated_arrival = minute_timestamp(stop_time_update.arrival.time)
+                        time_difference = estimated_arrival - time_now
+
+                        if (estimated_arrival > time_now and
+                            ((next_time_difference is None) or (time_difference < next_time_difference))):
+                                next_time_difference = time_difference
+                                closest_vehicle = vehicle_update
+                                closest_stop_time_update = stop_time_update
+                                closest_estimated_arrival = estimated_arrival
+
+    if closest_vehicle and closest_stop_time_update:
+        print(f"Trip ID: {closest_vehicle.trip.trip_id}")
+        print(f"Route ID: {closest_vehicle.trip.route_id}")
+        time_difference_minutes = int(next_time_difference.total_seconds() // 60)
+        if time_difference_minutes > 1:
+            print(f"Your Vehicle will be arriving in {time_difference_minutes} minutes")
+        else: # "Vehicle" name is temporary
+            print(f"Your Vehicle will be arriving in {time_difference_minutes} minute")
+        print(f"ETA: {closest_estimated_arrival}")
+        print(f"Stop ID: Beams Rd {closest_stop_time_update.stop_id}")
+        print(f"Arrival delay: {closest_stop_time_update.arrival.delay}")
+        print("-" * 40)
 
 
-
+tracker(bus_feed, bus_340, bus_stop)
+tracker(train_feed,train_route_id, station)
